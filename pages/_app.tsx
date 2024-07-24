6da0f90680ed9a50403a29bfbc4fa8bf1dead4af
mohdmushtaq <mohdmushtaq251@gmail.com>
2024-07-25 00:25:02 +0530

    added analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import React, { useState } from "react";
 import 'react-tippy/dist/tippy.css'; // Make sure to import the CSS file
-
+import { Analytics } from "@vercel/analytics/react"
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [miniState, setMiniState] = useState(false);
 
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
         <div className="app-container">
           <Component {...pageProps} />
           
-
+          <Analytics />
         </div>
       
     </SessionProvider>
