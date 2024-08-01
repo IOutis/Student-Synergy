99e857633ee97fa0834e6f986ce21314c22bf2be
Mohd Mushtaq <126546049+IOutis@users.noreply.github.com>
2024-08-01 20:01:50 +0530

    Update note.js

--- pages/services/note.js
+++ pages/services/note.js
@@ -75,7 +75,7 @@ export default function Note() {
             <DisplayEditor note={note} />
             <button onClick={()=>handleDelete(note._id)} >Delete</button>
             <button onClick={()=>{handleDownload(note.content)}}>Download DOCX</button>
-            <p>If the image you saved is not displayed then try "enabling editing mode".</p>
+            <p>If the image you saved is not displayed then try <em>enabling editing mode</em>.</p>
             </div>
         </div>
     );
