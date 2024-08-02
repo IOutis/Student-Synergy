a2dd26423630c29d6c46ae61e4f07b0272fac4f2
mohdmushtaq <mohdmushtaq251@gmail.com>
2024-08-03 02:04:33 +0530

    updated notes history componnent

--- components/NotesHistory.js
+++ components/NotesHistory.js
@@ -30,39 +30,43 @@ export default function NotesHistory() {
     }, [session]);
 
     if (!session) {
-        return (<div> <NavComp></NavComp>
-            <div style={{ display:"flex", justifyContent:"center", marginTop:"6vh"}}>
-            <p>Please sign in to view your tasks.</p></div>
-            </div>);
+        return (
+            <div>
+                <NavComp />
+                <div style={{ display: "flex", justifyContent: "center", marginTop: "6vh" }}>
+                    <p>Please sign in to view your tasks.</p>
+                </div>
+            </div>
+        );
     }
-    const  handleSubmit = async (id) =>{
-          router.push(`/services/note?id=${id}`);
+
+    const handleSubmit = async (id) => {
+        router.push(`/services/note?id=${id}`);
     }
 
     return (
-        <div className="overflow-y-scroll" style={{ maxHeight:"87vh"}}>
-    <h3 className='mt-3 pb-3'>Notes History</h3>
-    {notes.length ? (
-    <ul style={{ paddingBottom: "3px",}}>
-        {notes.map(note => (
-            <div className='h-[6vh]  bg-gray-200 hover:bg-gray-200 hover:scale-110 ' 
-            style={{ marginBottom:"3px",marginTop:"6px" }}
-            >
-            <li 
-                 // Changed cursor to pointer
-                style={{paddingLeft:"5vw",marginTop:"6px", paddingTop:"3px" }}
-                key={note._id}
-                onClick={() => handleSubmit(note._id)}
-            >
-                <p style={{ marginTop:"6px", }}>{note.title}</p>
-            </li>
-            </div>
-        ))}
-    </ul>
-) : (
-    <p>No Notes saved</p>
-)}
-</div>
-
+        <div className="overflow-y-scroll" style={{ maxHeight: "87vh" }}>
+            <h3 className='mt-3 pb-3'>Notes History</h3>
+            {notes.length ? (
+                <ul style={{ paddingBottom: "3px" }}>
+                    {notes.map(note => (
+                        <div
+                            key={note._id} // Add key prop here
+                            className='h-[6vh] bg-gray-200 hover:bg-gray-200 hover:scale-110'
+                            style={{ marginBottom: "3px", marginTop: "6px" }}
+                        >
+                            <li
+                                style={{ paddingLeft: "5vw", marginTop: "6px", paddingTop: "3px" }}
+                                onClick={() => handleSubmit(note._id)}
+                            >
+                                <p style={{ marginTop: "6px" }}>{note.title}</p>
+                            </li>
+                        </div>
+                    ))}
+                </ul>
+            ) : (
+                <p>No Notes saved</p>
+            )}
+        </div>
     );
 }
